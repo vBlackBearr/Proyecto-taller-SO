46687ac776be6405b56209590e9ec7400897c62e
vBlackBearr <Luispuentes.pgo@outlook.com>
2023-12-14 10:17:46 -0600

    x

--- index.php
+++ index.php
@@ -1,20 +1,66 @@
+<?php
+// Función para obtener la información de los puertos desde firewalld
+function obtenerInformacionPuertos() {
+    $comando = "firewall-cmd --list-ports";
+    $informacionPuertos = shell_exec($comando);
+
+    return $informacionPuertos;
+}
+
+// Función para procesar la acción del formulario
+function procesarAccion() {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $puerto = $_POST["puerto"];
+        $accion = $_POST["accion"];
+
+        // Validar y sanitizar la entrada del usuario
+
+        // Ejecutar comandos de firewalld
+        $comando = "firewall-cmd --$accion-port=$puerto --permanent";
+        $resultado = shell_exec($comando);
+
+        // Recargar firewalld para aplicar los cambios
+        $comando_recarga = "firewall-cmd --reload";
+        shell_exec($comando_recarga);
+
+        $mensaje = "Comando ejecutado: $comando";
+    }
+}
+
+// Obtener información de los puertos antes de procesar la acción
+$informacionPuertos = obtenerInformacionPuertos();
+
+// Procesar la acción del formulario
+procesarAccion();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de Puertos</title>
+    <title>Control de Puertos</title>
     <style>
         body {
             font-family: Arial, sans-serif;
             margin: 20px;
         }
-        h1 {
+        h1, h2 {
             color: #333;
+        }
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 20px;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 10px;
+            text-align: left;
         }
         form {
             max-width: 400px;
-            margin: 20px 0;
+            margin-top: 20px;
         }
         input, select {
             width: 100%;
@@ -31,7 +77,28 @@
 </head>
 <body>
     <h1>Control de Puertos</h1>
-    <form action="gestion_puertos.php" method="post">
+    
+    <h2>Estado Actual de los Puertos</h2>
+    <table>
+        <tr>
+            <th>Puerto</th>
+            <th>Estado</th>
+        </tr>
+        <?php
+        // Parsear y mostrar la información de los puertos
+        $lineasPuertos = explode("\n", trim($informacionPuertos));
+        foreach ($lineasPuertos as $linea) {
+            $datosPuerto = explode("/", $linea);
+            $numeroPuerto = $datosPuerto[0];
+            $estadoPuerto = $datosPuerto[1] ?? "Desconocido";
+            
+            echo "<tr><td>$numeroPuerto</td><td>$estadoPuerto</td></tr>";
+        }
+        ?>
+    </table>
+    
+    <h2>Realizar Acción</h2>
+    <form action="index.php" method="post">
         <label for="puerto">Número de Puerto:</label>
         <input type="text" name="puerto" required>
         
@@ -43,5 +110,10 @@
 
         <input type="submit" value="Enviar">
     </form>
+    
+    <?php if (isset($mensaje)) : ?>
+        <h2>Resultado de la Operación</h2>
+        <p><?php echo $mensaje; ?></p>
+    <?php endif; ?>
 </body>
 </html>
